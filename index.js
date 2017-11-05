17e83b17637c54770e3696e9ebb06c4a7f64cf2a
mazarul islam <complex.logik@gmail.com>
2017-11-05 17:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function happyHolidaysTo(name){
 function happyHolidays(){
   return `Happy holidays!`;
 }
+function happyHolidayTo(holiday, name){
+  return `Happy ${holidays}, ${name}!`;
+}
+function holidayCountdown(holidy, days){
+  return `It's ${days} days until ${holiday}!`;
+}
